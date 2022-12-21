4437e265e1f165f6d9970214d28007f129063604
chanhong-ji <jichanhong24@gmail.com>
2022-12-21 21:39:54 +0900

    chore: Set environ variables

---
+++ config/variables.py
@@ -0,0 +1,25 @@
+import environ
+from pathlib import Path
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+env = environ.Env()
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+
+def required(key, default_value=None):
+    try:
+        value = env(key)
+    except Exception:
+        if not default_value:
+            raise ImproperlyConfigured(f"Set the {key} environment variable")
+        value = default_value
+    return value
+
+
+varialbes = {
+    "django": {
+        "secret_key": required("SECRET_KEY"),
+    }
+}
