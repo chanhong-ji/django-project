a150bc16fe564fe1d7b5228f696052c070ccf5fe
chanhong-ji <jichanhong24@gmail.com>
2023-01-23 21:10:00 +0900

    chore

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,22 @@ import os
 import dj_database_url
 from pathlib import Path
 from config.variables import variables
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://06033b3f2bcd47f98403c6c40392a1f9@o4504553717563392.ingest.sentry.io/4504553718743040",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
